2d820c095d2756a9e1c9ca13dc3f6430fd9c93ef
mandranela <89pavel3@gmail.com>
2024-04-11 15:50:22 +0300

    proto templates now dont add timestamp import when not needed

--- protos/IOT4_package_pb2.py
+++ protos/IOT4_package_pb2.py
@@ -12,16 +12,15 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
-from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19protos/IOT4_package.proto\x12\x0cIOT4_package\x1a\x1fgoogle/protobuf/timestamp.proto\"\xfa\x01\n\x14IOT4_package_message\x12\x12\n\ncreated_at\x18\x01 \x01(\t\x12\x10\n\x08\x65ntry_id\x18\x02 \x01(\x03\x12\x14\n\x0ctemperaturec\x18\x03 \x01(\x02\x12\x14\n\x0cturbidityntu\x18\x04 \x01(\x03\x12\x1b\n\x13\x64issolved_oxygengml\x18\x05 \x01(\x02\x12\n\n\x02ph\x18\x06 \x01(\x02\x12\x12\n\nammoniagml\x18\x07 \x01(\x02\x12\x12\n\nnitrategml\x18\x08 \x01(\x03\x12\x12\n\npopulation\x18\t \x01(\x03\x12\x15\n\rfish_lengthcm\x18\n \x01(\x02\x12\x14\n\x0c\x66ish_weightg\x18\x0b \x01(\x02\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19protos/IOT4_package.proto\x12\x0cIOT4_package\"\xfa\x01\n\x14IOT4_package_message\x12\x12\n\ncreated_at\x18\x01 \x01(\t\x12\x10\n\x08\x65ntry_id\x18\x02 \x01(\x03\x12\x14\n\x0ctemperaturec\x18\x03 \x01(\x02\x12\x14\n\x0cturbidityntu\x18\x04 \x01(\x03\x12\x1b\n\x13\x64issolved_oxygengml\x18\x05 \x01(\x02\x12\n\n\x02ph\x18\x06 \x01(\x02\x12\x12\n\nammoniagml\x18\x07 \x01(\x02\x12\x12\n\nnitrategml\x18\x08 \x01(\x03\x12\x12\n\npopulation\x18\t \x01(\x03\x12\x15\n\rfish_lengthcm\x18\n \x01(\x02\x12\x14\n\x0c\x66ish_weightg\x18\x0b \x01(\x02\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'protos.IOT4_package_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
-  _globals['_IOT4_PACKAGE_MESSAGE']._serialized_start=77
-  _globals['_IOT4_PACKAGE_MESSAGE']._serialized_end=327
+  _globals['_IOT4_PACKAGE_MESSAGE']._serialized_start=44
+  _globals['_IOT4_PACKAGE_MESSAGE']._serialized_end=294
 # @@protoc_insertion_point(module_scope)
